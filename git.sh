a499f435f93eaf6e3875829f1f4c8cb9d00fb928
Richard Howe <howerj@aston.ac.uk>
2013-05-03 19:43:39 +0100

    Thesis: timing analysis

--- git.sh
+++ git.sh
@@ -1,4 +1,34 @@
-#/bin/bash
-git add .;
-git commit -m "$1";
-git push -u origin master;
+#!/bin/bash
+echo "Simple git add/commit/push script";
+
+if
+  [ -z "$1" ]
+then
+  echo "To use run type ./git.sh \"commit message\".";
+  echo "No commit message detected, exiting.";
+  exit 1;
+fi;
+
+if 
+  ! git add .; 
+then 
+  echo "\"git add .\" failed."; 
+  exit 1; 
+fi;
+
+if 
+  ! git commit -m "$1"; 
+then 
+  echo "\"git commit -m '$1'\" failed.";  
+  exit 1; 
+fi;
+
+if 
+  ! git push -u origin master; 
+then 
+  echo "\"git push -u origin master\" failed."; 
+  exit 1; 
+fi;
+
+echo "Git add/commit/push done.";
+exit 0;
