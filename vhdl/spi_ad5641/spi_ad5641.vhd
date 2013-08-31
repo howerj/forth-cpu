a101b6f0f1f048153fb0dc2aea25b54c10136cb0
Richard James Howe <howe.r.j.89@gmail.com>
2013-08-31 18:26:29 +0100

    Fleshing out spi driver

--- vhdl/spi_ad5641/spi_ad5641.vhd
+++ vhdl/spi_ad5641/spi_ad5641.vhd
@@ -1,17 +1,58 @@
 -- SPI, AD5641 Interface
+-- This SPI channel is output only.
+
 library ieee,work,std;
 use ieee.std_logic_1164.all; 
 use ieee.numeric_std.all;
 
 entity spi_ad5641 is
   port(
+    clk, rst: in std_logic;
 
+    ctrl_we:  in std_logic;                     -- Control register write enable
+    ctrl_reg: in std_logic_vector(15 downto 0); -- Control register
+
+    idata_we: in std_logic;
+    idata:    in std_logic_vector(15 downto 0); -- Input data
+    
+    cs, oclk, odata: out std_logic               -- SPI, output only
       );
 end entity;
 
 architecture rtl of spi_ad5641 is
-
+  signal idata_c, idata_n:       std_logic_vector(15 downto 0) := (others => '0');
+  signal ctrl_reg_c, ctrl_reg_n: std_logic_vector(15 downto 0) := (others => '0');
 begin
+  cs <= '0';
+  odata <= '0';
 
+  oclk <= clk;
 
+  registerProcess: process (clk,rst)
+  begin
+    if rst = '1' then
+      idata_c     <= (others => '0');
+      ctrl_reg_c  <= (others => '0');
+    elsif rising_edge(clk) then
+      idata_c     <= idata_n;
+      ctrl_reg_c  <= ctrl_reg_n;
+    end if;
+  end process registerProcess;
+
+  mainProcess: process(
+    idata, idata_c, idata_we,
+    ctrl_reg, ctrl_reg_c, ctrl_we
+  )
+  begin
+    idata_n    <= idata_c;
+    ctrl_reg_n <= ctrl_reg_c;
+
+    if idata_we = '1' then
+      idata_n  <= idata;
+    end if;
+
+    if ctrl_we = '1' then
+      ctrl_reg_n <= ctrl_reg;
+    end if;
+  end process mainProcess;
 end architecture;
---
+++ vhdl/spi_ad5641/spi_test_bench.vhd
@@ -0,0 +1,42 @@
+-- SPI AD5641 interface.
+
+library ieee,work,std;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+
+entity spi_test_bench is
+end entity;
+
+architecture simulation of spi_test_bench is
+    constant    clk_freq:        positive                        :=  1000000000;
+    constant    clk_period:      time                            :=  1000 ms / clk_freq;
+
+    signal      wait_flag:       std_logic                       :=  '0';
+    signal      tb_clk:          std_logic                       :=  '0';
+    signal      tb_rst:          std_logic;
+
+
+begin
+
+	clk_process: process
+	begin
+      while wait_flag = '0' loop
+			tb_clk	<=	'1';
+			wait for clk_period/2;
+			tb_clk	<=	'0';
+			wait for clk_period/2;
+        end loop;
+        wait;
+	end process;
+
+	stimulus_process: process
+	begin
+        tb_rst <= '1';
+        wait for clk_period * 2;
+        tb_rst <= '0';
+        wait for clk_period * 10000;
+        wait_flag   <=  '1';
+        wait;
+    end process;
+
+end architecture;
