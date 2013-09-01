2860cc30553f3b68c8b5a207bbbf86ffac90167b
Richard James Howe <howe.r.j.89@gmail.com>
2013-09-01 15:36:29 +0100

    SPI->AD5641 driver nearly done

--- vhdl/spi_ad5641/spi_ad5641.vhd
+++ vhdl/spi_ad5641/spi_ad5641.vhd
@@ -20,35 +20,56 @@ entity spi_ad5641 is
 end entity;
 
 architecture rtl of spi_ad5641 is
-  signal idata_c, idata_n:       std_logic_vector(15 downto 0) := (others => '0');
-  signal ctrl_reg_c, ctrl_reg_n: std_logic_vector(15 downto 0) := (others => '0');
+  signal idata_c, idata_n:        std_logic_vector(15 downto 0) := (others => '0');
+  signal ctrl_reg_c, ctrl_reg_n:  std_logic_vector(15 downto 0) := (others => '0');
+  signal enable_c, enable_n:      std_logic := '0';
+  signal done, sout_s:            std_logic := '0';
 begin
-  cs <= '0';
-  odata <= '0';
+  cs    <= enable_c;
+  oclk  <= clk    when enable_c = '1' else '0';
+  odata <= sout_s when enable_c = '1' else '0';
 
-  oclk <= clk;
+  shift_reg_16_instance: entity work.shift_reg_16
+    port map(
+      clk => clk,
+      rst => rst,
+      enable => enable_c,
+      sin => idata_c,
+      sout => sout_s,
+      done => done
+    );
 
   registerProcess: process (clk,rst)
   begin
     if rst = '1' then
       idata_c     <= (others => '0');
       ctrl_reg_c  <= (others => '0');
+      enable_c    <= '0';
     elsif rising_edge(clk) then
       idata_c     <= idata_n;
       ctrl_reg_c  <= ctrl_reg_n;
+      enable_c    <= enable_n;
     end if;
   end process registerProcess;
 
   mainProcess: process(
     idata, idata_c, idata_we,
-    ctrl_reg, ctrl_reg_c, ctrl_we
+    ctrl_reg, ctrl_reg_c, ctrl_we,
+    enable_c,
+    done
   )
   begin
     idata_n    <= idata_c;
     ctrl_reg_n <= ctrl_reg_c;
+    enable_n   <= enable_c;
 
     if idata_we = '1' then
       idata_n  <= idata;
+      enable_n <= '1';
+    end if;
+
+    if done = '1' then
+      enable_n <= '0';
     end if;
 
     if ctrl_we = '1' then
--- vhdl/spi_ad5641/spi_test_bench.vhd
+++ vhdl/spi_ad5641/spi_test_bench.vhd
@@ -8,35 +8,57 @@ entity spi_test_bench is
 end entity;
 
 architecture simulation of spi_test_bench is
-    constant    clk_freq:        positive                        :=  1000000000;
-    constant    clk_period:      time                            :=  1000 ms / clk_freq;
+    constant    clk_freq:     positive                        :=  1000000000;
+    constant    clk_period:   time                            :=  1000 ms / clk_freq;
 
-    signal      wait_flag:       std_logic                       :=  '0';
-    signal      tb_clk:          std_logic                       :=  '0';
-    signal      tb_rst:          std_logic;
+    signal      wait_flag:    std_logic                       :=  '0';
+    signal      tb_clk:       std_logic                       :=  '0';
+    signal      tb_rst:       std_logic                       :=  '1';
+    signal      tb_ctrl_we:   std_logic                       :=  '0';
+    signal      tb_ctrl_reg:  std_logic_vector(15 downto 0)   := (others => '0');
+    signal      tb_idata_we:  std_logic                       :=  '0';
+    signal      tb_idata:     std_logic_vector(15 downto 0)   := (others => '0');
 
-
+    signal      tb_cs:        std_logic                       :=  '0';
+    signal      tb_oclk:      std_logic                       :=  '0';
+    signal      tb_odata:     std_logic                       :=  '0';
 begin
+
+  spi_ad5641_uut: entity work.spi_ad5641
+  port map(
+    clk       => tb_clk,
+    rst       => tb_rst,
+    ctrl_we   => tb_ctrl_we,
+    ctrl_reg  => tb_ctrl_reg,
+    idata_we  => tb_idata_we,
+    idata     => tb_idata,
+    cs        => tb_cs,
+    oclk      => tb_oclk,
+    odata     => tb_odata
+          );
 
 	clk_process: process
 	begin
-      while wait_flag = '0' loop
-			tb_clk	<=	'1';
-			wait for clk_period/2;
-			tb_clk	<=	'0';
-			wait for clk_period/2;
-        end loop;
-        wait;
+    while wait_flag = '0' loop
+      tb_clk	<=	'1';
+      wait for clk_period/2;
+      tb_clk	<=	'0';
+      wait for clk_period/2;
+    end loop;
+    wait;
 	end process;
 
 	stimulus_process: process
 	begin
-        tb_rst <= '1';
-        wait for clk_period * 2;
-        tb_rst <= '0';
-        wait for clk_period * 10000;
-        wait_flag   <=  '1';
-        wait;
-    end process;
+    wait for clk_period * 2;
+    tb_rst <= '0';
+    tb_idata_we <= '1';
+    tb_idata <= X"8001";
+    wait for clk_period * 1;
+    tb_idata_we <= '0';
+    wait for clk_period * 256;
+    wait_flag   <=  '1';
+    wait;
+  end process;
 
 end architecture;
