fc0562242b430908f0290cb8452a3d1ac405ad9c
Richard James Howe <howe.r.j.89@gmail.com>
2013-09-05 22:14:16 +0100

    Fleshing out gptimer model

--- test/vhdl/timer/gptimer.vhd
+++ test/vhdl/timer/gptimer.vhd
@@ -27,9 +27,18 @@ entity gptimer is
 end entity;
 
 architecture rtl of gptimer is
-  signal ctr_r_c, ctr_r_n:      std_logic_vector(15 downto 0)  := (others => '0');
-  signal comp1_r_c, comp1_r_n:  std_logic_vector(15 downto 0)  := (others => '0');
-  signal comp2_r_c, comp2_r_n:  std_logic_vector(15 downto 0)  := (others => '0');
+  -- Registers for holding input
+  signal ctr_r_c, ctr_r_n:        std_logic_vector(15 downto 0)  := (others => '0');
+  signal comp1_r_c, comp1_r_n:    std_logic_vector(15 downto 0)  := (others => '0');
+  signal comp2_r_c, comp2_r_n:    std_logic_vector(15 downto 0)  := (others => '0');
+  -- Registers for holding output
+  signal timersig_c, timersig_n:  std_logic := '0';
+
+  -- Signals for dividing up the control register for clarity in simulation
+  signal ctrl_enabled:            std_logic := '0';
+  signal ctrl_updown:             std_logic := '0';
+  signal ctrl_comp1_action:       std_logic_vector(1 downto 0)   := (others => '0');
+  signal ctrl_comp2_action:       std_logic_vector(1 downto 0)   := (others => '0');
 begin
   clockRegisters: process(clk,rst)
   begin
@@ -37,21 +46,49 @@ begin
       ctr_r_c   <=  (others => '0');
       comp1_r_c <=  (others => '0');
       comp2_r_c <=  (others => '0');
+
+      timersig_c <= '0';
     elsif rising_edge(clk) then
       ctr_r_c   <=  ctr_r_n;
       comp1_r_c <=  comp1_r_n;
       comp2_r_c <=  comp2_r_n;
+
+      timersig_c <= timersig_n;
     end if;
   end process;
 
   assignRegisters: process( 
     ctr_r_we, comp1_r_we, comp2_r_we,
-    ctr_r_c, comp1_r_c, comp2_r_c
+    ctr_r_c, comp1_r_c, comp2_r_c,
+
+    timersig_c
   )
   begin
-      ctr_r_n   <=  ctr_r_c;
-      comp1_r_n <=  comp1_r_c;
-      comp2_r_n <=  comp2_r_c;
+  --- BEGIN Set register *default* next state BEGIN ---
+      timersig_n <= timersig_c;
+  --- END Set register *default* next state END ---
+
+  --- BEGIN Set register next state BEGIN ---
+
+      if ctr_r_we = '1' then
+        ctr_r_n   <=  ctr_r;
+      else
+        ctr_r_n   <=  ctr_r_c;
+      end if;
+
+      if comp1_r_we = '1' then
+        comp1_r_n   <=  comp1_r;
+      else
+        comp1_r_n   <=  comp1_r_c;
+      end if;
+
+      if comp2_r_we = '1' then
+        comp2_r_n   <=  comp2_r;
+      else
+        comp2_r_n   <=  comp2_r_c;
+      end if;
+  --- END Set register next state END ---
+
   end process;
 
 end architecture;
