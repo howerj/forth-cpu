894184d2fd77589f277c9f0e1a2cadc2ae6fdcc8
Richard James Howe <howe.r.j.89@gmail.com>
2013-09-05 21:03:44 +0100

    Finished timer template

--- test/vhdl/timer/gptimer.vhd
+++ test/vhdl/timer/gptimer.vhd
@@ -12,6 +12,11 @@ entity gptimer is
   port(
     clk:        in std_logic;
     rst:        in std_logic;
+
+    ctr_r_we:   in std_logic;                     -- ctr_r write enable
+    comp1_r_we: in std_logic;                     -- comp1_r write enable
+    comp2_r_we: in std_logic;                     -- comp2_r write enable
+
     ctr_r:      in std_logic_vector(15 downto 0); -- Control register
     comp1_r:    in std_logic_vector(15 downto 0); -- Compare value one
     comp2_r:    in std_logic_vector(15 downto 0); -- Compare value two
@@ -22,6 +27,31 @@ entity gptimer is
 end entity;
 
 architecture rtl of gptimer is
+  signal ctr_r_c, ctr_r_n:      std_logic_vector(15 downto 0)  := (others => '0');
+  signal comp1_r_c, comp1_r_n:  std_logic_vector(15 downto 0)  := (others => '0');
+  signal comp2_r_c, comp2_r_n:  std_logic_vector(15 downto 0)  := (others => '0');
 begin
+  clockRegisters: process(clk,rst)
+  begin
+    if rst = '1' then
+      ctr_r_c   <=  (others => '0');
+      comp1_r_c <=  (others => '0');
+      comp2_r_c <=  (others => '0');
+    elsif rising_edge(clk) then
+      ctr_r_c   <=  ctr_r_n;
+      comp1_r_c <=  comp1_r_n;
+      comp2_r_c <=  comp2_r_n;
+    end if;
+  end process;
+
+  assignRegisters: process( 
+    ctr_r_we, comp1_r_we, comp2_r_we,
+    ctr_r_c, comp1_r_c, comp2_r_c
+  )
+  begin
+      ctr_r_n   <=  ctr_r_c;
+      comp1_r_n <=  comp1_r_c;
+      comp2_r_n <=  comp2_r_c;
+  end process;
 
 end architecture;
--- test/vhdl/timer/gptimer_test_bench.vhd
+++ test/vhdl/timer/gptimer_test_bench.vhd
@@ -8,13 +8,36 @@ entity gptimer_test_bench is
 end entity;
 
 architecture simulation of gptimer_test_bench is
-    constant    clk_freq:     positive                        :=  1000000000;
-    constant    clk_period:   time                            :=  1000 ms / clk_freq;
+  constant clk_freq:     positive                        :=  1000000000;
+  constant clk_period:   time                            :=  1000 ms / clk_freq;
 
-    signal      wait_flag:    std_logic                       :=  '0';
-    signal      tb_clk:       std_logic                       :=  '0';
-    signal      tb_rst:       std_logic                       :=  '1';
+  signal   wait_flag:    std_logic                       :=  '0';
+  signal   tb_clk:       std_logic                       :=  '0';
+  signal   tb_rst:       std_logic                       :=  '1';
+
+  signal   tb_ctr_r_we:    std_logic :=  '0';
+  signal   tb_comp1_r_we:  std_logic :=  '0';
+  signal   tb_comp2_r_we:  std_logic :=  '0';
+  signal   tb_ctr_r:       std_logic_vector(15 downto 0) := (others =>'0'); 
+  signal   tb_comp1_r:     std_logic_vector(15 downto 0) := (others =>'0'); 
+  signal   tb_comp2_r:     std_logic_vector(15 downto 0) := (others =>'0'); 
+  signal   tb_interrupt:   std_logic := 'X';
+  signal   tb_timersig:    std_logic := 'X';                     
 begin
+
+  gpt_timer_uut: entity work.gptimer
+  port map(
+    clk         => tb_clk,
+    rst         => tb_rst,
+    ctr_r_we    => tb_ctr_r_we,
+    comp1_r_we  => tb_comp1_r_we,
+    comp2_r_we  => tb_comp2_r_we,
+    ctr_r       => tb_ctr_r,
+    comp1_r     => tb_comp1_r,
+    comp2_r     => tb_comp2_r,
+    interrupt   => tb_interrupt,
+    timersig    => tb_timersig
+          );
 
 	clk_process: process
 	begin
@@ -30,6 +53,8 @@ begin
 	stimulus_process: process
 	begin
     wait for clk_period * 256;
+
+    wait_flag <= '1';
     wait;
   end process;
 
