06a3fcbba798fc335e438d08fdb714b61da00ed6
Richard James Howe <howe.r.j.89@gmail.com>
2013-09-08 00:56:49 +0100

    Added path.sh

--- vhdl/path.sh
+++ vhdl/path.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-if [ ! -d /mnt/usb/opt ] then
+echo "This script just makes sure that my installation of Xilinx";
+echo "synthesis tools are on my path, it should be of no help to";
+echo "you. TODO, fix the problem and not use this work around...";
+if [ ! -d /mnt/usb/opt ]; then
   sudo mnt /mnt/sda2 /mnt/usb
 fi
-PATH=$PATH:/mnt/usb/opt/Xilinx/14.6/ISE_DS/ISE/lib/lin;
+PATH=$PATH:/mnt/usb/opt/Xilinx/14.6/ISE_DS/ISE/lib/lin/
+export PATH;
+echo $PATH;
+make simulation synthesis implementation bitfile
