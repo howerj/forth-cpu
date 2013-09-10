21dcbfeb71144f30a9b51eb7ea85f87639e516aa
Richard James Howe <howe.r.j.89@gmail.com>
2013-09-10 21:51:45 +0100

    Added CORDIC skeleton.

---
+++ test/vhdl/cordic/cordic.vhd
@@ -0,0 +1,22 @@
+-- Richard James Howe.
+--  CORDIC Top level
+--
+-- @author     Richard James Howe.
+-- @copyright    Copyright 2013 Richard James Howe.
+-- @license    LGPL    
+-- @email      howe.r.j.89@gmail.com
+
+library ieee,work;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cordic is
+  port(
+    clk:        in  std_logic;
+    rst:        in  std_logic
+  );
+end entity;
+
+architecture behav of cordic is
+begin
+end architecture;
