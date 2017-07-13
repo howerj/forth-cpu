4365f62aead070b0235fac71f74274bbd1c5a3eb
howerj <howe.r.j.89@gmail.com>
2017-07-13 21:39:44 +0100

    Minor changes in ucpu.vhd
    Getting read to merge with util.vhd

--- ucpu/ucpu.vhd
+++ ucpu/ucpu.vhd
@@ -37,52 +37,51 @@ entity ucpu is
 end entity;
 
 architecture rtl of ucpu is
-	signal a_c,   a_n:   std_logic_vector(di'high + 1 downto di'low) := (others => '0'); -- accumulator
-	signal pc_c,  pc_n:  std_logic_vector(pc'range)                  := (others => '0');
-	signal fetch_c, fetch_n: boolean := true; -- change this into something more meaningful
+	signal a_c,   a_n:       std_logic_vector(di'high + 1 downto di'low) := (others => '0'); -- accumulator
+	signal pc_c,  pc_n:      std_logic_vector(pc'range)                  := (others => '0');
+	signal alu:              std_logic_vector(1 downto 0)                := (others => '0');
+	signal state_c, state_n: std_logic                                   := '0'; 
 begin
 	pc  <= pc_c;
 	do  <= a_c(do'range);
+	alu <= op(op'high downto op'high - 1);
 
 	process(clk, rst)
 	begin
 		if rst = '1' then
 			pc_c    <= (others => '0');
 			a_c     <= (others => '0');
-			fetch_c <= true;
+			state_c <= '0';
 		elsif rising_edge(clk) then
 			pc_c    <= pc_n;
-			fetch_c <= fetch_n;
+			state_c <= state_n;
 			a_c     <= a_n;
 		end if;
 	end process;
 
-	process(op, di, a_c, pc_c, fetch_c)
-		variable alu: std_logic_vector(1 downto 0) := (others => '0'); -- turn into alias?
+	process(op, alu, di, a_c, pc_c, state_c)
 	begin
-		alu   := op(op'high downto op'high - 1);
 		we    <= '0';
 		a_n   <= a_c;
 		pc_n  <= pc_c;
 		re    <= '0';
 		adr   <= op(adr'range);
-		if fetch_c then
-			fetch_n <= false; -- Turn into fetch_n <= not alu(1) when store is moved
-			re      <= not alu(1); -- Fetch on ADD and NOR
+		if state_c = '0' then
+			state_n <= '1'; -- Turn into state_n <= not alu(1) when store is moved
+			re      <= not alu(1); -- state on ADD and NOR
 			pc_n    <= std_logic_vector(unsigned(pc_c) + 1);
 
 			if alu = "11" then -- JCC
 				if a_c(a_c'high) = '0' then 
 					a_n(a_n'high) <= '0'; -- clear carry
 					pc_n          <= op(pc_n'range); 
-					fetch_n       <= true;
 				end if;
 			end if;
 		else
-			fetch_n <= true;
+			state_n <= '0';
 			if    alu = "00" then a_n <= std_logic_vector(unsigned('0' & a_c(di'range)) + unsigned('0' & di));
 			elsif alu = "01" then a_n <= a_c nor ('0' & di);
-			elsif alu = "10" then we <= '1'; -- move to fetch, then ALU switch can be on lowest bit
+			elsif alu = "10" then we <= '1'; -- move to state, then ALU switch can be on lowest bit
 			else                  null;
 			end if;
 		end if;
