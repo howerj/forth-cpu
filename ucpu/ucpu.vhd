81d2c2961ec9155f007c61479d9323aa3d9a237c
howerj <howe.r.j.89@gmail.com>
2017-07-15 03:19:46 +0100

    Simplified ucpu.vhd
    Reduced line count.

--- ucpu/ucpu.vhd
+++ ucpu/ucpu.vhd
@@ -13,12 +13,13 @@
 --  STORE            1    10 ADDRESS MEM[ADDRESS] = A
 --  JCC              1    11 ADDRESS IF(CARRY) { PC = ADDRESS, CLEAR CARRY }
 --
--- @todo Make a single port RAM version
---
 -- It would be interesting to make a customizable CPU in which the
 -- instructions could be customized based upon what. Another interesting
 -- possibility is making a simple assembler purely in VHDL, which should
--- be possible, but difficult.
+-- be possible, but difficult. A single port version would require another
+-- state to fetch the operand and another register, or more states.
+--
+-- @todo Test in hardware
 --
 
 library ieee,work;
@@ -40,10 +41,10 @@ entity ucpu is
 end entity;
 
 architecture rtl of ucpu is
-	signal a_c,   a_n:               unsigned(di'high + 1 downto di'low) := (others => '0'); -- accumulator
-	signal pc_c,  pc_n, pc_plus_one: unsigned(pc'range)                  := (others => '0');
-	signal alu:                      std_logic_vector(1 downto 0)        := (others => '0');
-	signal state_c, state_n:         std_logic                           := '0'; 
+	signal a_c,   a_n:       unsigned(di'high + 1 downto di'low) := (others => '0'); -- accumulator
+	signal pc_c,  pc_n:      unsigned(pc'range)                  := (others => '0');
+	signal alu:              std_logic_vector(1 downto 0)        := (others => '0');
+	signal state_c, state_n: std_logic                           := '0'; 
 begin
 	pc          <= std_logic_vector(pc_n);
 	do          <= std_logic_vector(a_c(do'range));
@@ -51,8 +52,10 @@ begin
 	adr         <= op(adr'range);
 	we          <= '1' when alu = "10" else '0'; -- STORE
 	re          <= alu(1) nor state_c;           -- FETCH for ADD and NOR
-	pc_plus_one <= pc_c + 1;
 	state_n     <= alu(1) nor state_c;           -- FETCH not taken or FETCH done
+	pc_n        <= unsigned(op(pc_n'range)) when (alu = "11"    and a_c(a_c'high) = '0') else -- Jump when carry set
+			pc_c                    when (state_c = '0' and alu(1) = '0')        else -- FETCH
+			pc_c + 1; -- EXECUTE 
 
 	process(clk, rst)
 	begin
@@ -67,22 +70,13 @@ begin
 		end if;
 	end process;
 
-	process(op, alu, di, a_c, pc_c, pc_plus_one, state_c)
+	process(op, alu, di, a_c, state_c)
 	begin
 		a_n     <= a_c;
-		pc_n    <= pc_c;
 
-		if state_c = '0' then -- FETCH for ADD or NOT, EXECUTE JCC or STORE
-			if alu(1) = '1' then pc_n <= pc_plus_one; end if;
+		if alu = "11" and a_c(a_c'high) = '0' then a_n(a_n'high) <= '0'; end if;
 
-			if alu = "11" then -- JCC
-				if a_c(a_c'high) = '0' then 
-					a_n(a_n'high) <= '0'; -- clear carry
-					pc_n          <= unsigned(op(pc_n'range)); 
-				end if;
-			end if;
-		else -- EXECUTE for ADD and NOR
-			pc_n    <= pc_plus_one;
+		if state_c = '1' then -- EXECUTE for ADD and NOR
 			assert alu(1) = '0' severity failure;
 			if alu(0) = '0' then a_n <= '0' & a_c(di'range) + unsigned('0' & di); end if;
 			if alu(0) = '1' then a_n <= a_c nor '0' & unsigned(di); end if;
