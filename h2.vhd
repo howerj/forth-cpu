3335113b751595540a94db7c278e1dfe1e714918
Richard James Howe <howe.r.j.89@gmail.com>
2017-08-10 23:07:37 +0100

    Cleaning up "h2.vhd"
    "h2.vhd" has had minor cosmetic changes applied to it.

--- h2.vhd
+++ h2.vhd
@@ -53,24 +53,23 @@ end;
 
 architecture rtl of h2 is
 
-	-- Program counter.
-	signal pc_c:  std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(start_address, 13));
-	signal pc_n:  std_logic_vector(12 downto 0) := (others => '0');
+	signal pc_c:        std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(start_address, 13));
+	signal pc_n:        std_logic_vector(12 downto 0) := (others => '0');
+	signal pc_plus_one: std_logic_vector(12 downto 0) := (others => '0');
 
-	-- Stack Type
 	constant stack_size: integer := 2 ** stack_size_log2;
-	type     stack is array (stack_size - 1 downto 0) of std_logic_vector(15 downto 0);
-	subtype  depth is std_logic_vector(stack_size_log2 - 1 downto 0);
+	type     stack_type is array (stack_size - 1 downto 0) of std_logic_vector(15 downto 0);
+	subtype  depth is unsigned(stack_size_log2 - 1 downto 0);
 
-	-- Variable stack (RAM Template)
-	signal vstkp_c, vstkp_n:  depth := (others => '0');
-	signal vstk_ram: stack          := (others => (others => '0'));
-	signal dd: depth                := (others => '0'); -- variable stack delta
+	signal vstkp_c, vstkp_n:  depth := (others => '0');             -- variable stack pointer
+	signal vstk_ram: stack_type     := (others => (others => '0')); -- variable stack
+	signal dstk_we: std_logic       := '0';                         -- variable stack write enable
+	signal dd: depth                := (others => '0');             -- variable stack delta
 
-	-- Return stack (RAM Template)
-	signal rstkp_c, rstkp_n:  depth := (others => '0');
-	signal rstk_ram: stack          := (others => (others => '0'));
-	signal rd: depth                := (others => '0'); -- return stack delta
+	signal rstkp_c, rstkp_n:  depth := (others => '0');             -- return stack pointer
+	signal rstk_ram: stack_type     := (others => (others => '0')); -- return stack
+	signal rstk_we: std_logic       := '0';                         -- return stack write enable
+	signal rd: depth                := (others => '0');             -- return stack delta
 
 	type instruction_info_type is record
 		alu:     std_logic;
@@ -94,77 +93,53 @@ architecture rtl of h2 is
 
 	signal compare: compare_type := ('0', '0', '0', '0');
 
-	-- Interrupts
-	signal int_en_c, int_en_n:     std_logic :=  '0';
-	signal irq_c, irq_n:           std_logic :=  '0';
+	signal int_en_c, int_en_n:     std_logic :=  '0'; -- interrupt enable
+	signal irq_c, irq_n:           std_logic :=  '0'; -- interrupt request
 	signal irq_addr_c, irq_addr_n: std_logic_vector(interrupt_address_length - 1 downto 0) :=  (others => '0');
 
-	-- Top of stack, and next on stack.
-	signal tos_c, tos_n: std_logic_vector(15 downto 0) := (others => '0');
-	signal nos:          std_logic_vector(15 downto 0) := (others => '0');
-
-	-- Top of return stack.
-	signal rtos_c:       std_logic_vector(15 downto 0) := (others => '0');
-
-	-- aluop is what is fed into the alu.
-	signal aluop: std_logic_vector(4 downto 0)         := (others => '0');
-	signal pc_plus_one: std_logic_vector(12 downto 0)  := (others => '0');
-
-	-- Stack signals
-	signal dstk_we:   std_logic := '0';
-	signal rstk_we:   std_logic := '0';
-	signal rstk_data: std_logic_vector(15 downto 0) := (others => '0');
+	signal tos_c, tos_n: std_logic_vector(15 downto 0) := (others => '0'); -- top of stack 
+	signal nos:          std_logic_vector(15 downto 0) := (others => '0'); -- next on stack
+	signal rtos_c:       std_logic_vector(15 downto 0) := (others => '0'); -- top of return stack
+	signal rstk_data:    std_logic_vector(15 downto 0) := (others => '0'); -- return stack input
+	signal aluop:        std_logic_vector( 4 downto 0) := (others => '0'); -- ALU operation
 
 begin
-	assert stack_size > 4    report "stack size too small: " & integer'image(stack_size) severity failure;
+	assert stack_size > 4 report "stack size too small: " & integer'image(stack_size) severity failure;
 
-	-- instruction decoding is performed here
-	is_instr.alu     <=  '1' when insn(15 downto 13) = "011" else '0';
-	is_instr.lit     <=  '1' when insn(15) = '1' else '0';
-	is_instr.branch  <=  '1' when insn(15 downto 13) = "000" else '0';
-	is_instr.branch0 <=  '1' when insn(15 downto 13) = "001" else '0';
-	is_instr.call    <=  '1' when insn(15 downto 13) = "010" else '0';
-
-	is_interrupt     <=  '1' when irq_c = '1' and int_en_c = '1' and use_interrupts else '0';
-	is_ram_write     <=  '1' when is_instr.alu = '1' and insn(5) = '1' else '0';
-
-	compare.more  <= '1' when signed(tos_c)   > signed(nos)   else '0';
-	compare.umore <= '1' when unsigned(tos_c) > unsigned(nos) else '0';
-	compare.equal <= '1' when tos_c = nos else '0';
-	compare.zero  <= '1' when unsigned(tos_c(15 downto 0)) = 0 else '0';
-
-	-- Stack assignments
-	nos    <=  vstk_ram(to_integer(unsigned(vstkp_c)));
-	rtos_c <=  rstk_ram(to_integer(unsigned(rstkp_c)));
-
-	-- I/O assignments
-	pco    <=  pc_n;
-
-	dout   <=  nos;
-	daddr  <=  tos_c(13 downto 1) when is_ram_write = '1' else tos_n(13 downto 1);
-	dwe    <=  '1' when is_ram_write = '1' and tos_c(15 downto 14) = "00" else '0';
-	dre    <=  '1' when tos_n(15 downto 14) = "00" else '0';
-
-	io_dout   <=  nos;
-	io_daddr  <=  tos_c(15 downto 0);
-	io_wr     <= '1' when is_ram_write = '1' and tos_c(15 downto 14) /= "00" else '0';
-
-	pc_plus_one <= std_logic_vector(unsigned(pc_c) + 1);
-
-	-- Sign extend the stack deltas in the instruction
-	dd        <= (0 => insn(0), others => insn(1));
-	rd        <= (0 => insn(2), others => insn(3));
-
-	dstk_we     <= '1' when is_instr.lit = '1' or (is_instr.alu = '1' and insn(7) = '1') else '0';
+	is_instr.alu     <= '1' when insn(15 downto 13) = "011" else '0';
+	is_instr.lit     <= '1' when insn(15)           = '1'   else '0';
+	is_instr.branch  <= '1' when insn(15 downto 13) = "000" else '0';
+	is_instr.branch0 <= '1' when insn(15 downto 13) = "001" else '0';
+	is_instr.call    <= '1' when insn(15 downto 13) = "010" else '0';
+	is_interrupt     <= '1' when irq_c = '1' and int_en_c = '1' and use_interrupts else '0';
+	is_ram_write     <= '1' when is_instr.alu = '1' and insn(5) = '1' else '0';
+	compare.more     <= '1' when signed(tos_c)   > signed(nos)   else '0';
+	compare.umore    <= '1' when unsigned(tos_c) > unsigned(nos) else '0';
+	compare.equal    <= '1' when tos_c = nos else '0';
+	compare.zero     <= '1' when unsigned(tos_c(15 downto 0)) = 0 else '0';
+	nos              <= vstk_ram(to_integer(vstkp_c));
+	rtos_c           <= rstk_ram(to_integer(rstkp_c));
+	pco              <= pc_n;
+	pc_plus_one      <= std_logic_vector(unsigned(pc_c) + 1);
+	dout             <= nos;
+	daddr            <= tos_c(13 downto 1) when is_ram_write = '1' else tos_n(13 downto 1);
+	dwe              <= '1' when is_ram_write = '1' and tos_c(15 downto 14) = "00" else '0';
+	dre              <= '1' when tos_n(15 downto 14) = "00" else '0';
+	io_dout          <= nos;
+	io_daddr         <= tos_c(15 downto 0);
+	io_wr            <= '1' when is_ram_write = '1' and tos_c(15 downto 14) /= "00" else '0';
+	dd               <= (0 => insn(0), others => insn(1)); -- sign extend
+	rd               <= (0 => insn(2), others => insn(3)); -- sign extend
+	dstk_we          <= '1' when is_instr.lit = '1' or (is_instr.alu = '1' and insn(7) = '1') else '0';
 
 	stack_write: process(clk)
 	begin
 		if rising_edge(clk) then
 			if dstk_we = '1' then
-				vstk_ram(to_integer(unsigned(vstkp_n))) <= tos_c(15 downto 0);
+				vstk_ram(to_integer(vstkp_n)) <= tos_c(15 downto 0);
 			end if;
 			if rstk_we = '1' then
-				rstk_ram(to_integer(unsigned(rstkp_n))) <= rstk_data;
+				rstk_ram(to_integer(rstkp_n)) <= rstk_data;
 			end if;
 		end if;
 	end process;
@@ -179,12 +154,9 @@ begin
 			aluop <= insn(12 downto 8);
 		else
 			aluop <= (others => '0');
-			-- This assert fails at time = 0 ns
-			-- assert is_instr.lit = '1' report "undefined ALUOP";
 		end if;
 	end process;
 
-	-- ALU
 	alu: process(
 		is_instr.lit,
 		tos_c, nos, rtos_c,
@@ -195,11 +167,11 @@ begin
 		int_en_c,
 		stop)
 	begin
-		io_re          <=  '0'; -- hardware reads can have side effects
-		tos_n          <=  tos_c;
-		int_en_n       <=  int_en_c;
+		io_re    <=  '0';      -- hardware reads can have side effects
+		tos_n    <=  tos_c;
+		int_en_n <=  int_en_c;
 	if stop = '1' then
-		-- Do nothing
+		null;
 	elsif is_instr.lit = '1' then
 		tos_n   <=  "0" & insn(14 downto 0);
 	else
@@ -232,13 +204,13 @@ begin
 			end if;
 		when "01101" => tos_n <=  std_logic_vector(unsigned(nos) sll to_integer(unsigned(tos_c(3 downto 0))));
 		when "01110" => tos_n(15 downto 0) <= (others => '0');
-				tos_n(vstkp_c'range) <= vstkp_c;
+				tos_n(vstkp_c'range) <= std_logic_vector(vstkp_c);
 		when "01111" => tos_n    <= (others => compare.umore);
 		when "10000" => int_en_n <= tos_c(0);
 				-- tos_n    <= nos;
 		when "10001" => tos_n    <= (others => int_en_c);
 		when "10010" => tos_n(15 downto 0) <= (others => '0');
-				tos_n(rstkp_c'range) <= rstkp_c;
+				tos_n(rstkp_c'range) <= std_logic_vector(rstkp_c);
 		when "10011" => tos_n    <= (others => compare.zero);
 		when "10100" => tos_n    <= cpu_id;
 		when others  => tos_n <= tos_c;
@@ -247,7 +219,6 @@ begin
 	end if;
 	end process;
 
-	-- Reset and state-machine clock.
 	next_state: process(clk, rst)
 	begin
 		if rst = '1' then
@@ -280,49 +251,40 @@ begin
 		pc_plus_one,
 		stop)
 	begin
-		vstkp_n <= vstkp_c;
-		rstkp_n <= rstkp_c;
+		vstkp_n   <= vstkp_c;
+		rstkp_n   <= rstkp_c;
+		rstk_we   <= '0';
+		rstk_data <= "00" & pc_plus_one & "0";
 
-		-- main control
-		if stop = '1' then
-			-- Do nothing
+		if stop = '1' then -- Do nothing
 			rstk_we   <= '0';
 			rstk_data <= (others => '0');
-		elsif is_interrupt = '1' then
-			assert to_integer(unsigned(rstkp_c)) + 1 < stack_size;
+		elsif is_interrupt = '1' then -- Interrupts are similar to a call
+			assert to_integer(rstkp_c) + 1 < stack_size;
 
-			-- Interrupts are similar to a call
-			rstkp_n   <= std_logic_vector(unsigned(rstkp_c) + 1);
+			rstkp_n   <= rstkp_c + 1;
 			rstk_we   <= '1';
 			rstk_data <= "00" & pc_c & "0"; -- return to current instruction
 		elsif is_instr.lit = '1' then
-			assert to_integer(unsigned(vstkp_c)) + 1 < stack_size;
+			assert to_integer(vstkp_c) + 1 < stack_size;
 
-			vstkp_n   <= std_logic_vector(unsigned(vstkp_c) + 1);
+			vstkp_n   <= vstkp_c + 1;
 			rstk_we   <= '0';
 			rstk_data <= "00" & pc_plus_one & "0";
 		elsif is_instr.alu = '1' then
-			-- For return stack: implication insn(6) -> stack within bounds
-			assert (not insn(6) = '1') or ((to_integer(unsigned(rstkp_c)) + to_integer(signed(rd))) < stack_size);
-			assert                        ((to_integer(unsigned(vstkp_c)) + to_integer(signed(dd))) < stack_size);
+			assert (not insn(6) = '1') or ((to_integer(rstkp_c) + to_integer(signed(rd))) < stack_size);
+			assert                        ((to_integer(vstkp_c) + to_integer(signed(dd))) < stack_size);
 
 			rstk_we   <= insn(6);
 			rstk_data <= tos_c;
-			vstkp_n   <= std_logic_vector(unsigned(vstkp_c) + unsigned(dd));
-			rstkp_n   <= std_logic_vector(unsigned(rstkp_c) + unsigned(rd));
-		else
-			if is_instr.branch0 = '1' then
-				vstkp_n <= std_logic_vector(unsigned(vstkp_c) - 1);
-			end if;
-
-			if is_instr.call = '1' then
-				rstkp_n   <= std_logic_vector(unsigned(rstkp_c) + 1);
-				rstk_we   <= '1';
-				rstk_data <= "00" & pc_plus_one & "0";
-			else
-				rstk_we   <= '0';
-				rstk_data <= "00" & pc_plus_one & "0";
-			end if;
+			vstkp_n   <= vstkp_c + unsigned(dd);
+			rstkp_n   <= rstkp_c + unsigned(rd);
+		elsif is_instr.branch0 = '1' then
+			vstkp_n   <= vstkp_c - 1;
+		elsif is_instr.call = '1' then
+			rstkp_n   <= rstkp_c + 1;
+			rstk_we   <= '1';
+			rstk_data <= "00" & pc_plus_one & "0";
 		end if;
 	end process;
 
@@ -340,8 +302,8 @@ begin
 
 		if irq = '1' then irq_addr_n <= irq_addr; end if;
 
-		if stop = '1' then
-			-- Do nothing
+		if stop = '1' then 
+			null;
 		elsif is_interrupt = '1' then -- Update PC on interrupt
 			irq_n      <= '0';
 			irq_addr_n <= (others => '0');
